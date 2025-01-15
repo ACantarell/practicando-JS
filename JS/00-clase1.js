d2e7de837bb0584160926e3d104b9fb57a8b90ca
Amadeo <amadeocantarell@gmail.com>
2025-01-15 18:51:05 -0300

    agregando archivos nuevos de JS

---
+++ JS/00-clase1.js
@@ -0,0 +1,16 @@
+console.log("Hola Mundo");
+console.log('Hola Mundo');
+console.log(`Hola Mundo`);
+
+
+
+// <------ esto es un comentario en linea
+
+/*
+Esto es un comentario 
+pero 
+mas de una linea
+*/ 
+
+console.log(4)
+console.log("4")
