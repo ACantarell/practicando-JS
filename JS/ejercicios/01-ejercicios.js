e6400e2d8a1fce1e8cedfb4ead5e84e02d0ea489
Amadeo <amadeocantarell@gmail.com>
2025-01-20 09:03:17 -0300

    add folder and modifed file

---
+++ JS/ejercicios/01-ejercicios.js
@@ -0,0 +1,118 @@
+// 1. Escribe un comentario en una línea
+
+// 2. Escribe un comentario en varias líneas
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+
+// 4. Imprime por consola el valor de todas las variables
+
+// 5. Imprime por consola el tipo de todas las variables
+
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+
+// 9. A continuación, modifica los valores de las constantes
+
+// 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
+
+//Resultado de los ejercicios.
+
+// 1. Escribe un comentario en una línea
+//El resultado del primer ejercicio.
+
+// 2. Escribe un comentario en varias líneas
+/*
+2- Este es el
+segundo ejercicio
+resuleto.
+*/
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+/*
+3- datos primitivos:
+
+const string = "asd"
+const number = 123
+const booleans 
+const undefined
+const null
+const symbol = Symbol("mySimbol")
+const bigInt = 12312321312321312n
+*/
+
+// 4. Imprime por consola el valor de todas las variables
+/*
+const string = "asd"
+const number = 123
+const booleans = true
+let undefined
+const Null = null
+const symbol = Symbol("mySimbol")
+const bigInt = 1231231232131231231232131239435982378923749823744238n
+
+console.log(string);
+console.log(number);
+console.log(booleans);
+console.log(undefined);
+console.log(null);
+console.log(BigInt);
+console.log(symbol);
+*/
+
+// 5. Imprime por consola el tipo de todas las variables
+/*
+var variable1 = "var"
+let variable2 = "let"
+const variable3 = "const"
+console.log(variable1);
+console.log(variable2);
+console.log(variable3);
+*/
+
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+
+/*
+let variable = 123123
+variable = 456465
+console.log(variable);
+*/
+
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+
+/*
+let variable = "asdasdasd"
+variable = 123123123
+console.log(variable);
+*/
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+
+/*
+let estoEsString = "asdasda"
+let estoEsNumber = 123
+let estoEsBoolean = false
+let estoEsUndefined = undefined
+let estoEsNull = null
+let estoEsBigInt = BigInt(123123123112312312312312312)
+let estoEsSymbol = Symbol("123")
+console.log(estoEsString);
+console.log(estoEsNumber);
+console.log(estoEsBoolean);
+console.log(estoEsUndefined);
+console.log(estoEsNull);
+console.log(estoEsBigInt);
+console.log(estoEsSymbol);
+*/
+
+
+// 9. A continuación, modifica los valores de las constantes
+
+//No se puede.
+
+
+// 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
+
+//asdasdasdas
---
+++ JS/ejercicios/02-ejerciciosOperadores.js
@@ -0,0 +1,109 @@
+// 1. Crea una variable para cada operación aritmética
+
+/* 2. Crea una variable para cada tipo de operación de asignación,
+    que haga uso de las variables utilizadas para las operaciones aritméticas */
+
+// 3. Imprime 5 comparaciones verdaderas con diferentes operadores de comparación
+
+// 4. Imprime 5 comparaciones falsas con diferentes operadores de comparación
+
+// 5. Utiliza el operador lógico and
+
+// 6. Utiliza el operador lógico or
+
+// 7. Combina ambos operadores lógicos
+
+// 8. Añade alguna negación
+
+// 9. Utiliza el operador ternario
+
+// 10. Combina operadores aritméticos, de comparáción y lógicas
+
+
+
+// 1. Crea una variable para cada operación aritmética
+/*
+let a = 10
+let b = 20
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+*/
+
+//2. Crea una variable para cada tipo de operación de asignación,que haga uso de las variables utilizadas para las operaciones aritméticas
+/*
+let a = 11
+
+console.log(a += 2);
+console.log(a -= 2);
+console.log(a *= 2);
+console.log(a /= 2);
+console.log(a %= 2);
+console.log(a **=2);
+*/
+
+// 3. Imprime 5 comparaciones verdaderas con diferentes operadores de comparación
+/*
+let a = 10 
+let b = 20
+
+console.log(a < b);
+console.log(a <= b);
+console.log(!(a > b));
+console.log(a == 10);
+console.log(a === a);
+*/
+
+
+// 4. Imprime 5 comparaciones falsas con diferentes operadores de comparación
+/*
+let a = 10 
+let b = 20
+
+console.log(a > b);
+console.log(a >= b);
+console.log(!(a < b));
+console.log(a === "10");
+console.log(a !== a);
+*/
+
+// 5. Utiliza el operador lógico and
+/*let a = 10 
+let b = 20
+console.log(a > b && 20 < 30);
+*/
+
+// 6. Utiliza el operador lógico or
+/*
+let a = 10 
+let b = 20
+console.log(a > b || 20 > 30);
+*/
+
+
+// 7. Combina ambos operadores lógicos
+/*
+let a = 10 
+let b = 20
+console.log(a > b || 20 > 30);
+*/
+
+
+// 8. Añade alguna negación
+/*
+let a = 10 
+let b = 20 
+
+console.log(!(a < b || a > b ));
+console.log(!(a < b || a > b && a === "10"));
+*/
+
+// 9. Utiliza el operador ternario
+/*
+let muertoFerro = true
+muertoFerro ? console.log("Son y serán unos muertos por toda la vida ") : console.log("Son los mas grande del mundo, mas que el Real Madrid");
+
+*/
